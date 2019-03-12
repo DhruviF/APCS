6e78e9e012fb1e0a37f4e4cd6d574d25e25737b9
DhruviF <32720521+DhruviF@users.noreply.github.com>
2019-03-11 18:56:06 -0700

    Add files via upload

---
+++ GFEH/G.java
@@ -0,0 +1,15 @@
+/*
+Note that this class compiles and runs even with
+no explicit constructor
+*/
+public class G{
+	public void method1(){
+		System.out.println("G1");
+	}
+	public void method2(){
+		System.out.println("G2");
+	}
+	public String toString(){
+		return "G";
+	}
+}
---
+++ GFEH/GFEHClient.java
@@ -0,0 +1,58 @@
+/*
+Demonstrates Polymorphism at a more complex level in
+method1() call from class E.:
+	When called from class E, output is G1.
+	When called from class H, output is H1.
+*/
+
+public class GFEHClient{
+	public static void main(String[] args){
+		G[] elements = {new G(), new F(), new E(), new H()};
+		for (int i = 0; i < elements.length; i++){
+			System.out.println(elements[i]);
+			elements[i].method1();
+			elements[i].method2();
+			System.out.println();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Output:
+
+G
+G 1
+G 2
+
+F
+G 1
+F 2
+G 2
+
+F
+G 1
+E 2
+G 1
+
+F
+H 1
+E 2
+H 1
+
+*/
