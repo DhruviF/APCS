7b85e3420b81d60bbda9305c177a25dcab84cc76
DhruviF <32720521+DhruviF@users.noreply.github.com>
2019-03-11 18:54:46 -0700

    Add files via upload

---
+++ E.java
@@ -0,0 +1,12 @@
+/*  You are supplied with working code for GFEHClient and
+classes G, F, and H.  See if you can create class E such
+that the output of GFEHClient is: */
+
+public class E extends F{
+  public void method2(){
+    //prints the needed text
+    System.out.println("E2");
+    //calls the method
+    method1();
+  }
+}
